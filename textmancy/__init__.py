881f0a9748ce3bfa86c8b442ed2128557f191888
Caleb <caleb.case93@gmail.com>
2024-03-24 10:41:21 -0700

    Re-org inot components folder

--- textmancy/__init__.py
+++
@@ -1,6 +0,0 @@
-from .annotator import Annotator
-from .consolidator import Consolidator
-from .extractor import Extractor
-from .processor import Processor
-
-__all__ = ["Annotator", "Consolidator", "Extractor"]
